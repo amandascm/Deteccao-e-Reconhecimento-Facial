23f57a9253478019d2d0838735017e2aa22c9695
ascm <mandynhamoraes@hotmail.com>
2020-03-13 15:22:16 -0300

    menos custoso/deteccao mais precisa

--- Piloto.cpp
+++ Piloto.cpp
@@ -14,23 +14,19 @@ using namespace cv;
 
 String face_cascade_name = "haarcascade_frontalface_alt.xml";
 CascadeClassifier face_cascade;
-String window_name = "Capture-Face detection";
+String window_name = "Detecta faces";
 
 void detectAndDisplay(Mat frame){
-	std::vector<Rect> faces;
-    Mat frame_gray;
-    cvtColor( frame, frame_gray, COLOR_BGR2GRAY );
-    equalizeHist( frame_gray, frame_gray );
-    //-- Detect faces
-    face_cascade.detectMultiScale( frame_gray, faces, 1.1, 2, 0|CASCADE_SCALE_IMAGE, Size(30, 30) );
+	vector<Rect> faces; // vetor do tipo RECT (contém x, y, width e height)
+
+    //Detecta faces
+    face_cascade.detectMultiScale( frame, faces, 1.1, 2, 0|CASCADE_SCALE_IMAGE, Size(30, 30) );
     for ( size_t i = 0; i < faces.size(); i++ )
     {
         Point center( faces[i].x + faces[i].width/2, faces[i].y + faces[i].height/2 );
         ellipse( frame, center, Size( faces[i].width/2, faces[i].height/2 ), 0, 0, 360, Scalar( 255, 0, 255 ), 4, 8, 0 );
-        Mat faceROI = frame_gray( faces[i] );
-        std::vector<Rect> eyes;
     }
-    //-- Show what you got
+
     imshow( window_name, frame );
 }
 
@@ -42,19 +38,25 @@ int main (int argc, char** argv){
 
     if( !face_cascade.load( face_cascade_name ) ){ printf("--(!)Error loading face cascade\n"); return -1; };
 
-    //capture.open( -1 );
-    if ( ! capture.isOpened() ) { printf("--(!)Error opening video capture\n"); return -1; }
+    //capture.open( -1 ); Para detectar da webcam
+    if ( !capture.isOpened() ) {
+    	printf("Erro ao abrir o video\n"); 
+    	return -1;
+    }
     
-    cout << "AQUI CHEGOU\n";
-
     while(capture.read(frame)){
+
         if(frame.empty()){
-            printf(" --(!) No captured frame -- Break!");
+            printf("Impossivel ler frame\n");
             break;
         }
-        detectAndDisplay( frame );
+
+        detectAndDisplay(frame);
         char c = (char)waitKey(10);
-        if( c == 97 ) { break; } // escape
+
+        if( c == 32 ){
+       		break;
+       	} // interrompe reproducao do video ao clicar espaco
 	
 	}
 	return 0;
