16fa97b6b5c0dc528b370a0799f90a5caea43c10
ascm <mandynhamoraes@hotmail.com>
2020-03-13 16:20:23 -0300

    +Deteccao de perfis de faces

--- Piloto.cpp
+++ Piloto.cpp
@@ -13,19 +13,44 @@ using namespace std;
 using namespace cv;
 
 String face_cascade_name = "haarcascade_frontalface_alt.xml";
+String profile_face_cascade_name = "haarcascade_profileface.xml";
 CascadeClassifier face_cascade;
+CascadeClassifier profile_face_cascade;
 String window_name = "Detecta faces";
 
 void detectAndDisplay(Mat frame){
-	vector<Rect> faces; // vetor do tipo RECT (contém x, y, width e height)
+	vector<Rect> faces, profile_faces; // vetor do tipo RECT (contém x, y, width e height)
 
     //Detecta faces
     face_cascade.detectMultiScale( frame, faces, 1.1, 2, 0|CASCADE_SCALE_IMAGE, Size(30, 30) );
-    for ( size_t i = 0; i < faces.size(); i++ )
-    {
-        Point center( faces[i].x + faces[i].width/2, faces[i].y + faces[i].height/2 );
-        ellipse( frame, center, Size( faces[i].width/2, faces[i].height/2 ), 0, 0, 360, Scalar( 255, 0, 255 ), 4, 8, 0 );
-    }
+	profile_face_cascade.detectMultiScale( frame, profile_faces, 1.1, 2, 0|CASCADE_SCALE_IMAGE, Size(30, 30) );
+
+	if( faces.size() > profile_faces.size() ){
+
+		for( size_t i = 0; i < faces.size(); i++ ){
+			if( i < profile_faces.size() ){
+				Point center( profile_faces[i].x + profile_faces[i].width/2, profile_faces[i].y + profile_faces[i].height/2 );
+        		ellipse( frame, center, Size( profile_faces[i].width/2, profile_faces[i].height/2 ), 0, 0, 360, Scalar( 255, 255, 0 ), 4, 8, 0 );
+			}
+
+        	Point center( faces[i].x + faces[i].width/2, faces[i].y + faces[i].height/2 );
+        	ellipse( frame, center, Size( faces[i].width/2, faces[i].height/2 ), 0, 0, 360, Scalar( 255, 0, 255 ), 4, 8, 0 );
+    	}
+
+	} else{
+
+		for( size_t i = 0; i < profile_faces.size(); i++ ){
+
+			if( i < faces.size() ){
+				Point center( faces[i].x + faces[i].width/2, faces[i].y + faces[i].height/2 );
+        		ellipse( frame, center, Size( faces[i].width/2, faces[i].height/2 ), 0, 0, 360, Scalar( 255, 0, 255 ), 4, 8, 0 );
+			}
+
+       		Point center( profile_faces[i].x + profile_faces[i].width/2, profile_faces[i].y + profile_faces[i].height/2 );
+        	ellipse( frame, center, Size( profile_faces[i].width/2, profile_faces[i].height/2 ), 0, 0, 360, Scalar( 255, 255, 0 ), 4, 8, 0 );
+    	}		
+
+	}
 
     imshow( window_name, frame );
 }
@@ -41,14 +66,19 @@ int main (int argc, char** argv){
     	return -1;
     }
 
-    if ( !capture.isOpened() ) {
+    if( !profile_face_cascade.load( profile_face_cascade_name)){
+    	printf("Erro ao carregar profile face cascade\n");
+    	return -1;
+    }
+
+    if( !capture.isOpened() ) {
     	printf("Erro ao abrir o video\n"); 
     	return -1;
     }
     
-    while(capture.read(frame)){
+    while( capture.read(frame) ){
 
-        if(frame.empty()){
+        if( frame.empty() ){
             printf("Impossivel ler frame\n");
             break;
         }
@@ -61,6 +91,6 @@ int main (int argc, char** argv){
        	} // interrompe deteccao de faces e reproducao do video ao clicar espaco
 	
 	}
-	
+
 	return 0;
 }
