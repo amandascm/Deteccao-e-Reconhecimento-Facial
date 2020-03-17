cabeb8a67891d9ed05b8f8d15b673fd292a496c5
ascm <mandynhamoraes@hotmail.com>
2020-03-16 22:25:02 -0300

    Reconhece perfis de faces

--- Piloto.cpp
+++ Piloto.cpp
@@ -21,17 +21,34 @@ String profile_face_cascade_name = "haarcascade_profileface.xml";
 CascadeClassifier face_cascade;
 CascadeClassifier profile_face_cascade;
 String window_name = "Detecta faces";
-String window_name2 = "Reconhecido";
-String window_name3 = "Registrado";
+String window_name2 = "Reconhecid@";
+String window_name3 = "Registrad@";
 
 long int totalfaces = 0, totalprofilefaces = 0, total = 0;
-int im_width, im_height, num_componentes = 1;
+int im_width, im_height, num_componentes = 1, id = 23;
 double thresh = 10.0;
 
 Ptr<face::FaceRecognizer> reconhecer = face::createEigenFaceRecognizer(num_componentes, thresh);
 
-//----------------------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------------------------------------------------------------
 
+int reconhece(Mat resized_frame, Rect face){
+	Rect rectface = face;
+	Mat matface = resized_frame(rectface);
+	cvtColor(matface, matface, COLOR_BGR2GRAY);
+
+	Mat resizedmatface;
+	resize(matface, resizedmatface, Size(im_width, im_height), 1.0, 1.0, INTER_CUBIC);
+
+	int predicao = reconhecer->predict(resizedmatface);
+
+	if(predicao == id){
+		imshow(window_name2, resizedmatface);
+		return 1;
+	}else{
+		return 0;
+	}
+}
 
 void detectAndDisplay(Mat frame){
 
@@ -63,52 +80,41 @@ void detectAndDisplay(Mat frame){
 	if(faces.size() > profile_faces.size()){
 
 		for(size_t i = 0; i < faces.size(); i++){
-			if(i < profile_faces.size()){ //Elipse azul
+			if(i < profile_faces.size()){ 
 				Point center(profile_faces[i].x + profile_faces[i].width/2, profile_faces[i].y + profile_faces[i].height/2);
-        		ellipse(resized_frame, center, Size(profile_faces[i].width/2, profile_faces[i].height/2), 0, 0, 360, Scalar(255, 255, 0), 2, 8, 0);
+				//Foi reconhecid@?
+				if(reconhece(resized_frame, profile_faces[i])){ //Elipse verde
+        			ellipse(resized_frame, center, Size(profile_faces[i].width/2, profile_faces[i].height/2), 0, 0, 360, Scalar(0, 255, 0), 2, 8, 0);
+				}else{ //Elipse azul
+        			ellipse(resized_frame, center, Size(profile_faces[i].width/2, profile_faces[i].height/2), 0, 0, 360, Scalar(255, 255, 0), 2, 8, 0);
+				}
 			}
-
-			Rect rectface = faces[i];
-			Mat matface = resized_frame(rectface);
-			cvtColor( matface, matface, COLOR_BGR2GRAY );
-			Mat resizedmatface;
-
-			resize(matface, resizedmatface, Size(im_width, im_height), 1.0, 1.0, INTER_CUBIC);
-			int predicao = reconhecer->predict(resizedmatface);
-			//Foi reconhecido?
-			if(predicao == 23){ //Elipse verde
-				Point center(faces[i].x + faces[i].width/2, faces[i].y + faces[i].height/2);
+			//Foi reconhecid@?
+			Point center(faces[i].x + faces[i].width/2, faces[i].y + faces[i].height/2);
+			if(reconhece(resized_frame, faces[i])){ //Elipse verde
         		ellipse(resized_frame, center, Size(faces[i].width/2, faces[i].height/2), 0, 0, 360, Scalar(0, 255, 0), 2, 8, 0);
-        		imshow(window_name2, resizedmatface);
         	}else{ //Elipse rosa
-	        	Point center(faces[i].x + faces[i].width/2, faces[i].y + faces[i].height/2);
 	        	ellipse(resized_frame, center, Size(faces[i].width/2, faces[i].height/2), 0, 0, 360, Scalar(255, 0, 255), 2, 8, 0);
     		}
     	}
 	} else{
-
 		for(size_t i = 0; i < profile_faces.size(); i++){
 			if(i < faces.size()){
-				Rect rectface = faces[i];
-				Mat matface = resized_frame(rectface);
-				cvtColor( matface, matface, COLOR_BGR2GRAY );
-				Mat resizedmatface;
-
-				resize(matface, resizedmatface, Size(im_width, im_height), 1.0, 1.0, INTER_CUBIC);
-				int predicao = reconhecer->predict(resizedmatface);
-				//Foi reconhecido?
-				if(predicao == 23){ //Elipse verde
-					Point center(faces[i].x + faces[i].width/2, faces[i].y + faces[i].height/2);
+				Point center(faces[i].x + faces[i].width/2, faces[i].y + faces[i].height/2);
+				//Foi reconhecid@?
+				if(reconhece(resized_frame, faces[i])){ //Elipse verde
 	        		ellipse(resized_frame, center, Size( faces[i].width/2, faces[i].height/2), 0, 0, 360, Scalar(0, 255, 0), 2, 8, 0);
-	        		imshow(window_name2, resizedmatface);
 	        	}else{ //Elipse rosa
-					Point center(faces[i].x + faces[i].width/2, faces[i].y + faces[i].height/2);
 	        		ellipse(resized_frame, center, Size(faces[i].width/2, faces[i].height/2), 0, 0, 360, Scalar(255, 0, 255), 2, 8, 0);
 				}
 			}
-			//Elipse azul
-       		Point center(profile_faces[i].x + profile_faces[i].width/2, profile_faces[i].y + profile_faces[i].height/2);
-        	ellipse(resized_frame, center, Size( profile_faces[i].width/2, profile_faces[i].height/2 ), 0, 0, 360, Scalar(255, 255, 0), 2, 8, 0);
+			Point center(profile_faces[i].x + profile_faces[i].width/2, profile_faces[i].y + profile_faces[i].height/2);
+			//Foi reconhecid@?
+			if(reconhece(resized_frame, profile_faces[i])){ //Elipse verde
+        		ellipse(resized_frame, center, Size( profile_faces[i].width/2, profile_faces[i].height/2 ), 0, 0, 360, Scalar(0, 255, 0), 2, 8, 0);
+			}else{ //Elipse azul
+	        	ellipse(resized_frame, center, Size( profile_faces[i].width/2, profile_faces[i].height/2 ), 0, 0, 360, Scalar(255, 255, 0), 2, 8, 0);
+    		}
     	}		
 	}
 
@@ -116,12 +122,12 @@ void detectAndDisplay(Mat frame){
     imshow(window_name, resized_frame);
 }
 
-//----------------------------------------------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------------------------------------------------------------
 
 int main(int argc, char** argv){
 
 	VideoCapture capture("video.mp4");
-	Mat frame, img;
+	Mat frame, img, face;
 	vector<Rect> rect;
 	double fps;
 	int totalframes;
@@ -160,6 +166,7 @@ int main(int argc, char** argv){
 	Converte cor, encontra face, redimensiona e insere nos vetores imagens
 	Insere id's no vetor tags*/
 	for(int i=1; i<argc; i++){
+
 		img = imread(argv[i], IMREAD_GRAYSCALE);
 
 		if(img.empty()){
@@ -171,35 +178,33 @@ int main(int argc, char** argv){
 
 		face_cascade.detectMultiScale(img, rect, 1.1, 3, 0|CASCADE_SCALE_IMAGE, Size(30, 30));
 
-		Mat face = img(rect[i-1]);
+		face = img(rect[0]);
 
 		if(i==1){
 			im_width = face.cols;
 			im_height = face.rows;
 		}
 
-		resize(face, face, Size(im_width, im_height), 1.0, 1.0, INTER_CUBIC);
+		resize(face, face, Size(im_width, im_height), 1.0, 1.0, INTER_CUBIC); //Padroniza tamanho das matrizes
 
-		imshow(window_name3, face);
-
-		imagens.push_back(face);
+		imagens.push_back(face); //Registra faces no vetor de matrizes
 		//tags.push_back((i-1));
-		tags.push_back(23);
+		tags.push_back(id);
+		rect.clear();
 	}
 
-	//treina o reconhecedor de faces com os vetores imagens e tags
+	imshow(window_name3, face); //Printa última face registrada
+
+	//Treina o reconhecedor de faces com os vetores imagens e tags
     reconhecer->train(imagens, tags);
 
     	//Lê vídeo frame por frame, detecta faces e reconhece
     	while(capture.read(frame)){
-    		
 		    if(frame.empty()){
 		        printf( "Impossivel ler frame\n" );
 		        break;
 		    }
-
 		    detectAndDisplay(frame);
-
 		    //imshow( window_name, frame );
 		    char c = waitKey(1);
 		    if(c == 32){
